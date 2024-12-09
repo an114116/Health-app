109172624e085630316212dbfee255aaf74d63c4
an114116 <131609524+an114116@users.noreply.github.com>
2024-12-09 11:51:01 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(process.env.PORT || 4000);
+  await app.listen(process.env.PORT || 10000);
 }
 bootstrap();
